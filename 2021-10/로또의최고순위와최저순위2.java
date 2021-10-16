06685a587c40dfa167509871b9bbf1cf48694495
promise_yoo <56250078+ypr821@users.noreply.github.com>
2021-10-17 00:57:23 +0900

    로또의최고순위와최저순위

---
+++ 2021-10/로또의최고순위와최저순위2.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+class Solution {
+
+  public int[] solution(int[] lottos, int[] winNums) {
+    int[] answerss = new int[2];
+    int count = 0;
+    int zeroCount;
+    for (int lotto : lottos) {
+      if (lotto == 0) {
+        zeroCount++;
+      }
+    }
+
+    Arrays.sort(lottos);
+    Arrays.sort(winNums);
+    for (int winNum : winNums) {
+      for (int lotto : lottos) {
+        if (winNum == lotto) {
+          count++;
+        }
+      }
+    }
+
+    answers[0] = checkRanking(zeroCount + count); //높은순위
+    answers[1] = checkRanking(count); //낮은순위
+
+    return answers;
+  }
+
+  public int checkRanking(int count) {
+    if (count == 6) {
+      return 1;
+    } else if (count == 5) {
+      return 2;
+    } else if (count == 4) {
+      return 3;
+    } else if (count == 3) {
+      return 4;
+    } else if (count == 2) {
+      return 5;
+    } else {
+      return 6;
+    }
+  }
+}
